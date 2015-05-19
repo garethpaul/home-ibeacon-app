aa266bee211f3c236f948509531743df31fd1793
Gareth Paul Jones <gareth@garethpaul.com>
2015-05-18 21:59:12 -0700

    adding changes to view controller

--- HomeBeacon/ViewController.swift
+++ HomeBeacon/ViewController.swift
@@ -11,20 +11,12 @@ class ViewController: UIViewController{
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
     @IBOutlet weak var imageLoc: UIImageView!
+    var logoView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("get location")
-        println(appDelegate.currentLocation)
-        
-        let delayTime = dispatch_time(DISPATCH_TIME_NOW,
-            Int64(3 * Double(NSEC_PER_SEC)))
-        dispatch_after(delayTime, dispatch_get_main_queue()) {
-            if (self.appDelegate.currentLocation == "home") {
-                self.imageLoc.image = UIImage(named: "home.png")
-            }
-        }
-    
+        setupNav()
+        findLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -33,7 +25,35 @@ class ViewController: UIViewController{
         // Dispose of any resources that can be recreated.
     }
     
+    func setupNav() {
+        // SetupNav
+        logoView = UIImageView(frame: CGRectMake(0, 0, 40, 40))
+        logoView.image = UIImage(named: "logo")?.imageWithRenderingMode(.AlwaysTemplate)
+        logoView.tintColor = toColor("121212")
+        logoView.frame.origin.x = (self.view.frame.size.width - logoView.frame.size.width) / 2
+        logoView.frame.origin.y = 20
+        // Add the logo view to the navigation controller.
+        self.navigationController?.view.addSubview(logoView)
+        
+        // Bring the logo view to the front.
+        self.navigationController?.view.bringSubviewToFront(logoView)
+        
+        // Customize the navigation bar.
+        self.navigationController?.navigationBar.barTintColor = toColor("FF9800")
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
     
+    func findLocation() {
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW,
+            Int64(3 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            if (self.appDelegate.currentLocation == "home") {
+                self.imageLoc.image = UIImage(named: "home.png")
+            } else {
+                self.imageLoc.image = UIImage(named: "out.png")
+            }
+        }
+    }
 }
 
 
